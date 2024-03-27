ea252e08328c9792d13a91ef9094c65b3de54dff
henriquebazdev <165048951+henriquebazdev@users.noreply.github.com>
2024-03-27 16:52:26 -0300

    Novas funcionalidades
    - Criação de uma função de continuar a cadastrar usuários;
    
    - Formatação da exibição das informações do cadastrado na função de consulta;
    
    - Tratamento de exceções e opção de cancelar exclusão de usuário na função de deletar.

--- Cartorio.c
+++ Cartorio.c
@@ -3,6 +3,20 @@
 #include <locale.h> //biblioteca de alocação de texto por região
 #include <string.h> //biblioteca responsável por cuidar das strings
 
+int continuar_registro() //função para continuar a cadastrar usuários
+{	
+	int op = 0; //declaração da variável que será retornada
+	
+	system("cls"); //limpa tela
+	//menu de continuar cadastro
+	printf("\t1 - Continuar cadastrando usuários\n"); 
+	printf("\t2 - Voltar ao menu\n\n");
+	printf("Opção: "); 
+	scanf("%d", &op); //input da escolha do cliente
+	
+	return op; //returno da escolha
+}
+
 int registro() //Função responsável por cadastrar os usuários no sistema
 {
 	//inicio criação de variáveis/strings
@@ -11,52 +25,76 @@ int registro() //Função responsável por cadastrar os usuários no sistema
 	char nome[40];
 	char sobrenome[40];
 	char cargo[40];
+	int continuar = 0;
 	//final criação de variáveis
 	
 	printf("Digite o CPF a ser cadastrado: "); //coletando informação do usuário
 	scanf("%s", cpf); //%s refere-se a strings
-	
+		
 	strcpy(arquivo, cpf); //responsável por copiar os valores das strings
-	
+		
 	FILE *file; //cria o arquivo
 	file = fopen(arquivo, "w"); //cria o arquivo e o "w" significa escrever
 	fprintf(file, cpf); //salva o valor da variável
 	fclose(file); //fecha o arquivo
-	
+		
 	file = fopen(arquivo, "a"); //cria o arquivo e o "a" significa atualizar
 	fprintf(file, ","); //salva uma vírugula para separar os dados do aluno
 	fclose(file); //fecha o arquivo
-	
+		
 	printf("Digite o nome a ser cadastrado: "); //coletando informação do usuário
 	scanf("%s", nome); //%s refere-se a strings
-	
+		
 	file = fopen(arquivo, "a"); //abre o aquivo 
 	fprintf(file, nome); //salva o nome do aluno 
 	fclose(file); //fecha o arquivo
-	
+		
 	file = fopen(arquivo, "a"); //abre o arquivo para atualizar
 	fprintf(file, ","); //vírgula para separar os dados
 	fclose(file); //fecha o arquivo
-	
+		
 	printf("Digite o sobrenome a ser cadastrado: "); //coleta o sobrenome do aluno
 	scanf("%s", sobrenome); //salva na variável sobrenome
-	
+		
 	file = fopen(arquivo, "a"); //abre o arquivo para atualizar
 	fprintf(file, sobrenome); //salva no aquivo o sobrenome do aluno
 	fclose(file); //fecha o arquivo
-	
+		
 	file = fopen(arquivo, "a"); //abre o arquivo para atualizar
 	fprintf(file, ","); //salva uma vírugula para separar o nome do sobrenome do aluno
 	fclose(file); //fecha o arquivo
-	
+		
 	printf("Digite o cargo a ser cadastrado: "); //coleta o cargo do aluno
 	scanf("%s", cargo); //guarda na variável cargo
-	
+		
 	file = fopen(arquivo, "a"); //abre o arquivo dentro na variável file
 	fprintf(file, cargo); //salva o valor da variável cargo no arquivo
 	fclose(file); //fecha o arquivo
+		
+	printf("CPF cadatrado com sucesso!\n\n");
 	
 	system("pause"); //pausa o sistema
+	
+	continuar = continuar_registro(); //variável continuar recebe o retorno da função continuar registro
+	
+	switch (continuar) //condicional que decide o fluxo de código a partir da escolha do cliente
+	{
+		//opção 1: volta para a função de registro limpando a tela
+		case 1:
+			system("cls");
+			registro(); 
+			break;
+		//volta para o menu
+		case 2:
+			return 0;
+			break;
+		//tratamento de exceção: caso o cliente digite um opção inválida
+		default:
+			printf("\nOpção inválida! Voltando ao menu...\n\n"); //considera voltar ao menu como opção
+			system("pause");
+			return 0;
+			break;
+	}
 }
 
 int consulta() //função para consultar 
@@ -79,8 +117,22 @@ int consulta() //função para consultar
 	
 	while(fgets(conteudo, 200, file) != NULL) //lê todo o conteúdo do arquivo cpf que foi encontrado
 	{
-		printf("\nEssas são as informações do usuário: "); //título para apresentação dos dados do aluno
-		printf("%s", conteudo); //toda a variável conteúdo com as informações de cadastro do aluno
+		char *t;
+		
+		printf("\nEssas são as informações do usuário: \n\n"); //título para apresentação dos dados do aluno
+		//formatação em linhas das informações do usuário
+		t = strtok(conteudo, ","); //busca na string conteúdo a primeira "," e print tudo o que foi lido antes	
+		printf("CPF: %s\n", t); //printa essa informação
+		
+		t = strtok(NULL, ","); //busca a próxima "," da onde parou de ler
+		printf("Nome: %s\n", t); //printa essa informação
+		
+		t = strtok(NULL, ","); //busca a próxima "," da onde parou de ler
+		printf("Sobrenome: %s\n", t); //printa essa informação
+		
+		t= strtok(NULL, ","); //busca a próxima "," da onde parou de ler
+		printf("Cargo: %s\n", t); //printa essa informação na tela
+		
 		printf("\n\n"); //duas linhas em branco
 	}
 	
@@ -92,24 +144,40 @@ int consulta() //função para consultar
 int deletar() //função de deletar um cadastro da base de dados
 {
 	char cpf[40]; //cria a string cpf com 40 espaços
+	char resposta[] = "Sim";
+	int remover = 0;
 	
 	printf("Digite o CPF do usuário a ser deletado: "); //coleta o cpf a ser deletado
 	scanf("%s", cpf); //guarda o cpf na variável criada
+	//pergunta se realmente deseja deletar aceitando somente "Sim" ou "Não" como resposta
+	printf("Deseja realmente excluir este CPF do sistema? 'Sim' ou 'Não'? ");
+	scanf("%s", resposta); //guarda a escolha do cliente
 	
-	remove(cpf); //remove o cpf da base de dados
-		
-	FILE *file; //abre a função de arquivos
-	file = fopen(cpf, "r"); //abre o arquivo do cpf que pode ter sido deletado
+	if (strcmp(resposta, "Sim") != 0) //usa a função strcmp para comparar as strings e ver se retorna: 0 para Sim ou != 0 para Não
+	{ //caso a resposta seja != 0 é igual a Não
+		printf("\n\nA operação de cancelamento de usuário foi cancelada! \n"); //a operação é cancelada
+		printf("Voltando ao menu... \n\n"); //mensagem de voltando ao menu
+		system("pause"); //pausa o sistema
+		return 0; //retorna 0 para acabar com a função
+	}
+	else //caso a resposta seja Sim (strcmp(resposta, "Sim" == 0))
+	{	
+		remover = remove(cpf); //joga o valor da função remove(cpf) para dentro da variável remover
+		//se remover for igual a 0 = sucesso!
+		//se remover for igual a NULL == o usuário não existe!
+		if (remover == 0)
+		{
+			printf("Usuário deletado com sucesso do sistema! \n");
+			system("pause");
+		}
+		else 
+		{
+			printf("Usuário não se encontra no sistema! \n");
+			system("pause");
+		}
 	
-	if (file == NULL) //se o cpf foi deletado
-	{
-		printf("O usuário foi deletado com sucesso do sistema!\n");
-		system("pause");
-	} 
-	else //caso ele não exista 
-	{
-		printf("O usuário não se encontra no sistema!.\n");
-		system("pause");
+		FILE *file; //abre a função de arquivos
+		file = fopen(cpf, "r"); //abre o arquivo do cpf que pode ter sido deletado	
 	}
 }
 
